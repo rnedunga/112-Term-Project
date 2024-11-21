3a3a9348ad21a1e74bba6db86f7bc783e2764e36
Ravi Oliveira Nedungadi <rnedunga@andrew.cmu.edu>
2024-11-20 19:11:45 -0500

    IMPLEMENTED RAYCASTING!

--- main.py
+++ main.py
@@ -6,6 +6,17 @@ import pyaudio
 import numpy as np
 from scipy.signal import find_peaks
 
+def distance(x1, y1, x2, y2):
+    return ((x1-x2)**2 + (y1-y2)**2)**0.5
+
+def roundInt(x1):
+    base = int(x1)
+    if(base == 0):
+        base = 1
+    if((x1%base) > 0.5):
+        return base+1
+    return int(x1)
+
 class map:
     def __init__(self, blockSize = app.width):
         self.blockSize = blockSize
@@ -26,7 +37,7 @@ class map:
                     blocks.add(block)
                     self.objectBlocks[block] = self.objectBlocks.get(block, set())
                     self.objectBlocks[block].add(object)
-            print(self.objectBlocks)
+            #print(self.objectBlocks)
 
     def addEnemy(self, enemy):
         if(isinstance(enemy, Enemy)):
@@ -40,7 +51,7 @@ class map:
                     blocks.add(block)
                     self.enemyBlocks[block] = self.enemyBlocks.get(block, set())
                     self.enemyBlocks[block].add(object)
-            print(self.enemyBlocks)
+            #print(self.enemyBlocks)
 
     def getBlocks(self, edgeList):
         blockList = set()
@@ -66,11 +77,11 @@ class map:
         blocks = self.getBlocks(other.getEdges())
         hasCollided = False
         for block in blocks:
-            print(self.objectBlocks.get(block, []), end='')
+            #print(self.objectBlocks.get(block, []), end='')
             for object in self.objectBlocks.get(block, []):
                 if(object.checkCollision(other)):
                     hasCollided = True
-        print()
+        #print()
         return hasCollided
 
     def checkInteraction(self, other, radius):
@@ -85,6 +96,19 @@ class map:
                 for dy in {radius, -radius}:
                     if(object.checkCollision((other.x, other.y+dy, other.width, other.height))):
                         object.interact(other)
+                        return True
+                        
+    def checkHit(self, other, radius):
+        otherX, otherY, otherWidth, otherHeight = other
+        blocks = self.getBlocks([(otherX, otherY)])
+        hasCollided = False
+        for block in blocks:
+            for object in self.objectBlocks.get(block, []):
+                for dx in {radius, -radius}:
+                    if(object.checkCollision((otherX+dx, otherY, otherWidth, otherHeight))):
+                        return True
+                for dy in {radius, -radius}:
+                    if(object.checkCollision((otherX, otherY+dy, otherWidth, otherHeight))):
                         return True
 
     def enemiesFollowPlayer(self, app):
@@ -143,10 +167,12 @@ class MapObject:
                 return True
 
     def collide(self, other):
-        print(f'{self} collided w/ {other}')
+        pass
+        #print(f'{self} collided w/ {other}')
 
     def interact(self, other):
-        print(f'<{other} interacted w/ {self}')
+        pass
+        #print(f'<{other} interacted w/ {self}')
 
 class ReadableObject(MapObject):
     def __init__(self, x, y, height = None, width = None, radius = None, color = None, shape = None, message=''):
@@ -164,6 +190,8 @@ class Enemy:
         self.height = height
         self.sprite = sprite
         self.speed = speed
+        self.visionLimit = 300
+        self.visionBlocked = False
 
     def draw(self, app):
         drawRect(self.x - app.camX, self.y - app.camY, self.width, self.height, fill='lightGreen')
@@ -178,6 +206,15 @@ class Enemy:
         return [(self.x, self.y), (self.x + self.width, self.y), (self.x, self.y - self.height), (self.x + self.width, self.y + self.height)]
 
     def followPlayer(self, app):
+        if(app.step % 30 == 0):
+            if(self.canSee(app, app.player)):
+                self.visionBlocked = False
+            else:
+                self.visionBlocked = True
+
+        if(self.visionBlocked):
+            return
+
         target = (app.player.x, app.player.y)
         distanceToTarget = ((target[0]-self.x)**2 + (target[1]-self.y)**2)**0.5
         dx = (self.speed/distanceToTarget) * (target[0]-self.x)
@@ -187,7 +224,56 @@ class Enemy:
     def move(self, dx, dy):
         self.x += dx
         self.y += dy
-        
+
+    def canSee(self, app, other):
+        tileSize = 64
+        selfX, selfY = self.x + self.width/2, self.y + self.height/2
+        otherX, otherY = other.x + other.width/2, other.y + other.height/2
+        distX = otherX - selfX
+        distY = otherY - selfY
+        dist = distance(selfX, selfY, otherX, otherY)
+        if(dist <= 1):
+            return True
+        dir = (distX/dist, distY/dist)
+
+        normX, normY = 1,1
+        if(otherX < selfX): normX = -1
+        if(otherY < selfY): normY = -1
+
+        if(dist < self.visionLimit):
+            curCheck = (selfX, selfY)
+            while True:
+                distToX = normX*curCheck[0] % tileSize
+                distToY = normY*curCheck[1] % tileSize
+
+                distToX = tileSize - distToX
+                distToY = tileSize - distToY
+
+                distXToEdge = distToX
+                distYToEdge = distToY
+
+                if(distX != 0):
+                    distXToEdge = distance(0, 0, distToX, distToX*(distY/distX))
+                if(distY != 0):
+                    distYToEdge = distance(0, 0, distToY*(distX/distY), distToY)
+                
+                if distXToEdge < distYToEdge:
+                    curCheck = (curCheck[0] + (normX*distToX), curCheck[1] + normY * (distToX * abs(dir[1] / dir[0])))
+                else:
+                    curCheck = (curCheck[0] + normX*(distToY * abs(dir[0] / dir[1])), curCheck[1] + (normY*distToY))
+
+                
+                curCheck = (roundInt(curCheck[0]), roundInt(curCheck[1]))
+
+                if(distance(self.x, self.y, curCheck[0], curCheck[1]) > dist):
+                    return True
+                if app.map.checkHit((curCheck[0], curCheck[1], 1, 1), 1):
+                    return False
+        else:
+            return False
+                    
+
+
             
 
 class Player:
@@ -200,7 +286,8 @@ class Player:
         self.speed = speed
 
     def draw(self, app):
-        drawRect(self.x - app.camX, self.y - app.camY, self.width, self.height, fill='black')
+        drawImage(self.sprite, self.x - app.camX, self.y - app.camY)
+        #drawRect(self.x - app.camX, self.y - app.camY, self.width, self.height, fill='black')
 
     def move(self, app, keys):
         dx = 0
@@ -273,7 +360,7 @@ def evaluatePitch(app):
         app.freqList.append(peak_freq)
         app.noteList.pop(0)
         app.noteList.append(note)
-        print(f"Detected note: {app.note} (Frequency: {app.frequency} Hz)")
+        #print(f"Detected note: {app.note} (Frequency: {app.frequency} Hz)")
     
     if(app.freqList[0] == app.freqList[1] == app.freqList[2]):
         app.frequency = app.freqList[0]
@@ -283,7 +370,10 @@ def evaluatePitch(app):
 
 def onAppStart(app):
 
+    app.width = 800
+    app.height = 800
     app.step = 0
+    app.stepMode = True
 
     app.audio = pyaudio.PyAudio()
     app.stream = app.audio.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=1024)
@@ -308,7 +398,7 @@ def onAppStart(app):
     app.spell = ''
     initializeSpells(app)
     initializeMap(app)
-    app.player = Player(app.width/2 - 16, app.height/2 - 16, 32, 32, sprite='Sprites/Mage-1.png', speed=3)
+    app.player = Player(app.width/2 - 16, app.height/2 - 16, 32, 32, sprite='C:\CMU/Classes/15112/TermProject/Sprites/Mage-1.png', speed=3)
 
 def initializeSpells(app):
     app.spells = {'Dash':['blue'], 'Fireball':['red', 'green', 'red'], 'Thunder':['blue', 'red']}
@@ -319,6 +409,10 @@ def initializeMap(app):
     app.map.addObject(ReadableObject(0, 0, shape='rect', width=50, height=50, color='maroon', message="I'm a bookshelf!"))
     app.map.addObject(MapObject(50, 320, shape='circle', radius=20, color='purple'))
     app.map.addEnemy(Enemy(300, 300, 32, 32, sprite='', speed=2))
+    app.map.addEnemy(Enemy(200, 200, 32, 32, sprite='', speed=2))
+    app.map.addEnemy(Enemy(100, 100, 32, 32, sprite='', speed=2))
+    app.map.addEnemy(Enemy(0, 0, 32, 32, sprite='', speed=2))
+
 
 def onKeyPress(app, key):
     if(key == 'r'):
@@ -333,16 +427,20 @@ def onKeyPress(app, key):
         app.readCommand = not app.readCommand
     elif(key == 'e'):
         app.player.interact(app)
+    elif(key == 'p'):
+        app.stepMode = not app.stepMode
+    elif(key == 'l'):
+        takeStep(app)
 
 def onKeyHold(app, keys):
     app.player.move(app, keys)
 
 def redrawAll(app):
-    drawLabel('Press "R" to open mic and "S" to close',200, 20)
+    drawLabel('Press "R" to open mic and "S" to close',400, 20)
     app.map.draw(app)
     app.player.draw(app)
-    drawLabel(app.message, 200, 200, size = 20)
-    drawLabel(app.frequency, 200, 240, size = 20)
+    #drawLabel(app.message, 200, 40, size = 20)
+    #drawLabel(app.frequency, 200, 240, size = 20)
     drawLabel(app.spell, 200, 300, size = 15)
     drawMeter(app)
     drawCommand(app)
@@ -409,6 +507,10 @@ def readCommand(app):
 
 
 def onStep(app):
+    if(not app.stepMode):
+        takeStep(app)
+
+def takeStep(app):
     app.step += 1
     evaluatePitch(app)
     evaluateColor(app)
